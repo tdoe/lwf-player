18aa6471cd45a282d6a8da0357075c4db18c6d0a
tdoe <tdoe1985@gmail.com>
2014-05-10 23:55:53 +0900

    Add test.

---
+++ spec/lwfStageContractorSpec.js
@@ -0,0 +1,27 @@
+/**
+ * Created by tdoe on 5/10/14.
+ */
+
+describe("StageContractor class test", function () {
+
+
+    var playerSettings = new LwfPlayer.PlayerSettings();
+    playerSettings.targetStage = document.createElement("div");
+
+    var lwfSettings = new LwfPlayer.LwfSettings();
+    lwfSettings.lwf = "hoge.lwf";
+    lwfSettings.fitForWidth = true;
+
+    var player = new LwfPlayer.Player(playerSettings, lwfSettings);
+
+    var stageContractor = player.getStageContractor();
+
+    it("test getDevicePixelRatio", function () {
+        expect(stageContractor.getDevicePixelRatio()).toMatch(/\d/);
+    });
+
+    it("test getStageScale", function () {
+        stageContractor.changeStageSize(320, 480);
+        expect(stageContractor.getStageScale()).toEqual(1);
+    });
+});
